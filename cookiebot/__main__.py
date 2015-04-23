3cbcfc85e92dbca4ff95dc163f5457f7eefb8a73
MysteriousMagenta <therealrebuilder@gmail.com>
2015-04-23 18:16:54 +0200

    Config fallback

--- cookiebot/__main__.py
+++ cookiebot/__main__.py
@@ -8,6 +8,14 @@ except ImportError:
 from selenium.webdriver import *
 driver = Chrome
 config_name = "config.txt"
-with open(config_name) as config_file:
-    config = (cookiebot or cookiebot_config).parse_file(config_file)["CookieBot"]
+# If somebody forgets to rename it.
+fallback_config = "config-sample.txt"
+try:
+    config_file = open(config_name)
+except FileNotFoundError:
+    config_file = open(fallback_config)
+
+config = (cookiebot or cookiebot_config).parse_file(config_file)["CookieBot"]
+config_file.close()
+
 (cookiebot or cookiebot_bot).main(driver, config)
