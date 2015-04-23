f9537b418c8580cb22a08b88b8469c7648c26b90
MysteriousMagenta <therealrebuilder@gmail.com>
2015-04-23 18:52:52 +0200

    More small changes

--- cookiebot/cookie_selenium.py
+++ cookiebot/cookie_selenium.py
@@ -69,10 +69,9 @@ class CookieBot(object):
             things = filter(lambda x: x is not None, [best_building, best_upgrade])
             optimal = min(things, key=lambda x: x["ratio"])
             if optimal is not None:
-                self.echo("[+] [{}] Buying {} with price {} and mps {}".format(optimal["ratio"],
-                                                                               optimal["name"],
-                                                                               optimal["price"],
-                                                                               optimal["mps"]))
+                self.echo("[+] Buying {} with price {} and mps {}".format(optimal["name"],
+                                                                          optimal["price"],
+                                                                          optimal["mps"]))
                 if optimal["price"] > money:
                     difference = optimal["price"] - money
                     self.echo("[-] Missing {} money!".format(difference))
